3f5cd898bf3ec258dc38c95687db86004c6750aa
sifiabdelwaheb <abdelwahebsifi5@gmail.com>
2023-06-05 23:47:32 +0100

    add formation button

--- src/MainQuiz.js
+++ src/MainQuiz.js
@@ -2,7 +2,8 @@ import React, { useState, useEffect } from "react";
 import Quiz from "./Quiz";
 import quiz from "./questions";
 import { useDispatch, useSelector } from "react-redux";
-
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
 function MainQuiz() {
   const redux = useSelector((state) => state);
   const topicname = redux.test.tests.skills;
@@ -13,19 +14,60 @@ function MainQuiz() {
       {questionsWithTopic.length > 0 ? (
         <Quiz data={questionsWithTopic} />
       ) : (
-        <div
-          style={{
-            display: "flex",
-            justifyContent: "center",
-            alignContent: "center",
-            backgroundColor: "#f5f5f5",
-            paddingTop: "4%",
-            fontSize: "22px",
-          }}
-        >
-          Désolé . Nous sommes actuellement en train de préparer les questions
-          pour le test sur le sujet de{" "} {'   '} <span> </span> 
-          <div style={{ fontWeight: "bold" ,paddingLeft:"5px"}}> {topicname}</div>.
+        <div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignContent: "center",
+              backgroundColor: "#f5f5f5",
+              paddingTop: "4%",
+              fontSize: "22px",
+            }}
+          >
+            Désolé . Nous sommes actuellement en train de préparer les questions
+            pour le test {"   "} <span> </span>
+            <div style={{ fontWeight: "bold", paddingLeft: "5px" }}>
+              {" "}
+              {topicname}
+            </div>
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignContent: "center",
+              backgroundColor: "#f5f5f5",
+              paddingTop: "4%",
+              fontSize: "22px",
+            }}
+          >
+            . Nous vous recommandons de vous suivre la formation en ligne afin
+            d'améliorer vos connaissances et vos compétences Clickez sur le
+            button :
+          </div>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              alignContent: "center",
+              backgroundColor: "#f5f5f5",
+              paddingTop: "4%",
+              fontSize: "22px",
+            }}
+          >
+            <Link to={"/"}>
+              <Button
+                style={{
+                  borderRadius: "22px",
+                  maxHeight: "50px",
+                }}
+                variant="contained"
+              >
+                formation
+              </Button>
+            </Link>
+          </div>
         </div>
       )}
     </div>
